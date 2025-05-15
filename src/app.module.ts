2d63c5755e5dd16faeabfc751be1889f457d219b
sicoding1120 <daffahafizhfirdaus07@gmail.com>
2025-05-15 08:53:49 +0700

    new config mysql

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './app/auth/auth.module';
-import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [AuthModule, PrismaModule],
+  imports: [
+    AuthModule,
+    ConfigModule.forRoot({
+      isGlobal: true, // konfigurasi is global untuk semua module
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: async () => {
+        const { typeOrmConfig } = await import('../config/typeorm.config');
+        return typeOrmConfig;
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
