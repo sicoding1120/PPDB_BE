a9b2af68cdef4b026a642f1bec96923894e76b44
sicoding1120 <daffahafizhfirdaus07@gmail.com>
2025-05-21 09:45:19 +0700

    fix v5

---
+++ prisma/migrations/20250521024158_update/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE "Father" ALTER COLUMN "dateOfBirth" SET DATA TYPE TEXT;
+
+-- AlterTable
+ALTER TABLE "Mother" ALTER COLUMN "dateOfBirth" SET DATA TYPE TEXT;
+
+-- AlterTable
+ALTER TABLE "Student" ALTER COLUMN "dateOfBirth" SET DATA TYPE TEXT;
